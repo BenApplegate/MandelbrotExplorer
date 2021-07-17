2218dea92d036d44f597682f6dcae1867c72d574
BenApplegate <ben@benjaminapplegate.com>
2021-07-17 17:55:59 -0600

    Created blank form with needed events

--- MandelbrotExplorer/Program.cs
+++ MandelbrotExplorer/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace MandelbrotExplorer
 {
     class Program
     {
+        private static Form _form;
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _form = new Form();
+            _form.Size = new Size(1920, 1080);
+            _form.Paint += OnPaint;
+            _form.MouseWheel += OnScroll;
+            _form.KeyDown += KeyDownEvent;
+            
+            Application.Run(_form);
+        }
+
+        static void KeyDownEvent(object sender, KeyEventArgs e)
+        {
+            
+        }
+        static void OnScroll(object sender, MouseEventArgs e)
+        {
+            
+        }
+        static void OnPaint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
